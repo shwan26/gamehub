f9b1693b52473ae0b3135b636841e42344a6fbfe
Shwan <syanshwan.26@gmail.com>
2024-09-28 15:12:02 +0700

    fix library page

--- game-hub/src/components/Library.jsx
+++ game-hub/src/components/Library.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import users from '../data/users.json'; // Adjusted import to match your JSON structure
 import games from '../data/games.json';
+import GameCard from '../components/GameCard'; // Import the GameCard component
 
 const Library = () => {
   const currentUser = users[0]; 
@@ -11,17 +12,17 @@ const Library = () => {
 
   return (
     <div>
-      <h1>Library</h1>
-      <div className="game-grid">
+      <h1 className="text-left mb-4">Library</h1>
+  
+      <div className="row">
         {ownedGames.length > 0 ? (
           ownedGames.map((game) => (
-            <div key={game.id}>
-              <img src={game.image} alt={game.title} />
-              <h2>{game.title}</h2>
+            <div className="col-md-4 mb-4" key={game.id}>
+              <GameCard game={game} /> {/* Use the GameCard component */}
             </div>
           ))
         ) : (
-          <p>No games found in your library.</p> // Display a message if no games are owned
+          <p className="text-center">No games found in your library.</p> // Display a message if no games are owned
         )}
       </div>
     </div>
